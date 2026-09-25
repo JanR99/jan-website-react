11fb70b2ba7f94818c3eb12aa053fc89358f9b57
Jan <jan.rehnert@hotmail.de>
2026-09-25 10:21:39 +0200

    added firstName and lastName property

--- backend/src/main/java/de/jan/user/User.java
+++ backend/src/main/java/de/jan/user/User.java
@@ -14,7 +14,12 @@ public class User implements DatastoreEntity {
 
     @Index
     private String email;
+
     private String password;
+
+    private String firstname;
+
+    private String lastname;
 
     public User() {}
 
@@ -45,5 +50,21 @@ public class User implements DatastoreEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 }
